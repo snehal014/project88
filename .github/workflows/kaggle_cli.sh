2f2511c84645df51d019bf9417fd31463f3ded93
Snehal Sonawane <168650324+snehal014@users.noreply.github.com>
2024-08-06 17:03:06 +0530

    Update kaggle_cli.sh

--- .github/workflows/kaggle_cli.sh
+++ .github/workflows/kaggle_cli.sh
@@ -8,7 +8,7 @@ pip3 install kaggle
 
 # Configure Kaggle API (Replace with your Kaggle API key)
 mkdir -p ~/.kaggle
-echo '{"username":"snehal014","key":"bbfd26856d5590df1d43dcb74b10c93e"}' > ~/.kaggle/kaggle.json
+echo '{"username":"snehal_014","key":"bbfd26856d5590df1d43dcb74b10c93e"}' > ~/.kaggle/kaggle.json
 
 # Download the dataset from Kaggle
 kaggle datasets download -d username/dataset
